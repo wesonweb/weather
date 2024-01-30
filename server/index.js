1e476538bff5cab1f97808df0373897d02342810
Wes Mehaffy <wesonweb@gmail.com>
2024-01-30 14:32:03 +0000

    updates package.json file

--- server/index.js
+++ server/index.js
@@ -23,12 +23,12 @@ app.post('/', async (req, res) => {
     console.log(err)
   }
 })
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/dist')))
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/dist', 'index.html'))
-  })
-}
+// if (process.env.NODE_ENV === 'production') {
+//   app.use(express.static(path.join(__dirname, '../client/dist')))
+//   app.get('*', (req, res) => {
+//     res.sendFile(path.join(__dirname, '../client/dist', 'index.html'))
+//   })
+// }
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`)
