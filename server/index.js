66ff492d8009655b55df7f7611691685164bbb9a
Wes Mehaffy <wesonweb@gmail.com>
2024-01-30 13:52:29 +0000

    updates package.json file

--- server/index.js
+++ server/index.js
@@ -24,9 +24,9 @@ app.post('/', async (req, res) => {
   }
 })
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')))
+  app.use(express.static(path.join(__dirname, '../client/dist')))
   app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/build', 'index.html'))
+    res.sendFile(path.join(__dirname, '../client/dist', 'index.html'))
   })
 }
 
